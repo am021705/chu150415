e62dced772ca86d0124123ba1ec1a4acbbe43358
AM021698 <chu_tae@hotmail.com.tw>
2015-04-19 15:35:06 +0800

    AM021698

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Test
+{
+	public static void main (String [] args)
+	{
+		System.out.println("資工二A	AM021698	朱泰瑞");
+		System.out.println("資工二A	AM021698	朱泰瑞");
+		System.out.println("資工二A	AM021698	朱泰瑞");
+	}
+}
